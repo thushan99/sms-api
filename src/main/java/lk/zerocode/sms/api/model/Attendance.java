6071e58f5604cc5cee7cf89c83da47d8871fdde0
thushan99 <m.t.miuranga@gmail.com>
2024-12-23 16:53:19 +0530

    attendance

--- src/main/java/lk/zerocode/sms/api/model/Attendance.java
+++ src/main/java/lk/zerocode/sms/api/model/Attendance.java
@@ -3,8 +3,11 @@ package lk.zerocode.sms.api.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
-@Entity
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 @Data
+@Entity
 @Table(name = "attendances")
 public class Attendance extends BaseEntity {
 
@@ -13,6 +16,17 @@ public class Attendance extends BaseEntity {
     private Long id;
 
     @ManyToOne
+    private Grade grade;
+
+    @ManyToOne
     private Student student;
 
+    private LocalDate date;
+
+    private LocalTime time;
+
+    @Enumerated(EnumType.STRING)
+    private AttendanceStatus status;
+
+    private String remarks;
 }
---
+++ src/main/java/lk/zerocode/sms/api/model/AttendanceStatus.java
@@ -0,0 +1,8 @@
+package lk.zerocode.sms.api.model;
+
+public enum AttendanceStatus {
+
+    PRESENT,
+    ABSENT,
+    LATE
+}
